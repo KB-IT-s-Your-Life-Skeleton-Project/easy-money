1960c6476569c58621e9583f7168b2fc958efbf3
LEE HYUN JONG <hjong1010@dongguk.edu>
2026-04-07 14:34:40 +0900

    feat:공통 버튼 생성

---
+++ src/components/common/CommonButton.vue
@@ -0,0 +1,29 @@
+<script setup>
+// 사용 방법:
+// import CommonButton from "./components/common/CommonButton.vue";
+// <CommonButton text="텍스트 입력" @click=/>
+defineProps({
+  text: { type: String, default: "버튼" },
+});
+</script>
+
+<template>
+  <button>{{ text }}</button>
+</template>
+
+<style scoped>
+button {
+  background-color: #ffbc00;
+  border-radius: 10px;
+  border: none;
+  color: white;
+  width: 70%;
+  height: 43px;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  justify-self: center;
+  font-size: 1em;
+  cursor: pointer;
+}
+</style>
