75d57117fd2b63ea0155d7260fea15b542947eda
LEE HYUN JONG <hjong1010@dongguk.edu>
2026-04-08 15:36:20 +0900

    feat: tabBar UI 개발

--- src/App.vue
+++ src/App.vue
@@ -1,8 +1,11 @@
 <script setup>
-import Header from './components/Header.vue';
+import Header from "./components/Header.vue";
+import TabBar from "./components/TabBar.vue";
+const isVisible = false;
 </script>
 
 <template>
-  <Header v-if="false" />
+  <Header v-if="isVisible" />
+  <TabBar v-if="isVisible" />
   <router-view></router-view>
 </template>
