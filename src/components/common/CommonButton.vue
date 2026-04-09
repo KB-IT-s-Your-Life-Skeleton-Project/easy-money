69f0aac8e94e21d74fd54f40ebdbd25ec34b772f
yuna0833 <parkyuna0833@gmail.com>
2026-04-09 11:16:03 +0900

    feat: 거래 내역 추가 페이지 구현

--- src/components/common/CommonButton.vue
+++ src/components/common/CommonButton.vue
@@ -3,12 +3,17 @@
 // import CommonButton from "./components/common/CommonButton.vue";
 // <CommonButton text="텍스트 입력" @click=/>
 defineProps({
-  text: { type: String, default: "버튼" },
+  text: { type: String, default: '버튼' },
 });
+
+// 클릭 이벤트를 부모 컴포넌트로 전달하기 위해 정의
+defineEmits(['click']);
 </script>
 
 <template>
-  <button>{{ text }}</button>
+  <button :type="type" @click="$emit('click', $event)">
+    {{ text }}
+  </button>
 </template>
 
 <style scoped>
@@ -16,7 +21,7 @@ button {
   background-color: var(--color-yellow-dark);
   border-radius: 10px;
   border: none;
-  color: white;
+  color: rgb(0, 0, 0);
   width: 70%;
   height: 43px;
   display: flex;
