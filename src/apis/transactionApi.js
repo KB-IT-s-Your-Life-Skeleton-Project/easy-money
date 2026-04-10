fea078605f893dc13e56a40320a9c0d2feb043d7
LEE HYUN JONG <hjong1010@dongguk.edu>
2026-04-10 11:28:08 +0900

    feat: trasaction put,delete 추가

--- src/apis/transactionApi.js
+++ src/apis/transactionApi.js
@@ -1,6 +1,28 @@
 import axios from "axios";
 
 const BASE_URL = "/api/transactions";
+
+const createTransaction = async (data) => {
+  try {
+    const response = await axios.post(BASE_URL, data);
+    return response.data;
+  } catch (e) {
+    console.log("## 저장에 오류가 발생했습니다.");
+    if (e instanceof Error) console.log(e.message);
+    else console.log(e);
+  }
+};
+const deleteTransaction = async (id) => {
+  try {
+    const response = await axios.delete(`${BASE_URL}/${id}`);
+    return response.data;
+  } catch (e) {
+    console.log("삭제 중 오류가 발생하였습니다.");
+    if (e instanceof Error) console.log(e.message);
+    else console.log(e);
+  }
+};
+
 /**
  *
  * @returns 개별 거래내역 return
@@ -9,26 +31,23 @@ const BASE_URL = "/api/transactions";
 export const getTransactionById = async (id) => {
   try {
     const response = await axios.get(`${BASE_URL}/${id}`);
-    if (response.status === 200) {
-      console.log(response.data);
-      return response.data;
-    } else {
-      alert("거래내역 상세 조회 실패");
-    }
+    return response.data;
   } catch (err) {
     alert(`getTransaction 호출 에러: ${err.message}`);
   }
 };
+
 /**
  * 전체 거래, 특정 거래일, 수입/지출, 카테고리 입력으로 호출
  * 세부 쿼리는 store에서 작성
  * @param {*} options 조건 선택적으로 조회(example 참고)
  * @example
- * const all = await getTransactions();
+ * const all = await getTransactions({userId:1});
  * @example
- * const byDate = await getTransactions({ date: "2026-04-01" });
+ * const byDate = await getTransactions({ userId:1,date: "2026-04-01" });
  * @example
  * const byRange = await getTransactions({
+ * userId:1
  * startDate: "2026-04-01",
  * endDate: "2026-04-30",
  * category: ["음식", "교통"],
@@ -36,17 +55,16 @@ export const getTransactionById = async (id) => {
  * });
  **/
 
-export const getTransactions = async ({
+export const getTransactions = async (
   userId,
-  date,
-  startDate,
-  endDate,
-  category = [],
-  type,
-} = {}) => {
+  { date, startDate, endDate, category = [], type } = {},
+) => {
+  if (!userId) throw new Error("userId는 필수입니다.");
   try {
     const params = {};
-    if (userId) params.userId = userId;
+    if (userId) {
+      params.userId = userId;
+    }
     if (date) params.date = date;
     if (startDate && endDate) {
       params["date_gte"] = startDate;
@@ -62,6 +80,13 @@ export const getTransactions = async ({
     alert(`거래내역 호출 에러: ${err.message}`);
   }
 };
-export const updateTranscation = async () => {
-  response = await axios.put();
+export const updateTransaction = async (id, newData) => {
+  try {
+    const transaction = await getTransactionById(id);
+    const payload = { ...transaction, ...newData };
+    const response = await axios.put(`${BASE_URL}/${id}`, payload);
+    return response.data;
+  } catch (err) {
+    alert(`거래 수정 에러: ${err.message}`);
+  }
 };
